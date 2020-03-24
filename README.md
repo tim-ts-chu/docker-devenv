06ee97fc4e739683f9ef8a0b95f86a5c5c5eda0a
Tim Chu <timchu@TimMacAir.local>
2020-03-24 16:05:53 -0400

    add cuda layers into devenv

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 image hierarchy:
 
 * devbase
-* cuda10.2
+* cuda10.2-base
+* cuda10.2-runtime
+* cuda10.2-cudnn7-runtime
+* cuda10.2-devel
+* cuda10.2-cudnn7-devel
 
--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build . -f Dockerfile.devbase -t timtschu/dev:devbase
-docker build . -f Dockerfile.cuda10.2 -t timtschu/dev:cuda10.2
+#docker build . -f Dockerfile.devbase -t timtschu/devenv:devbase
+
+# cuda tool kits
+#docker build . -f cuda/Dockerfile.10.2-base -t timtschu/devenv:cuda10.2-base
+#docker build . -f cuda/Dockerfile.10.2-runtime -t timtschu/devenv:cuda10.2-runtime
+#docker build . -f cuda/Dockerfile.10.2-cudnn7-runtime -t timtschu/devenv:cuda10.2-cudnn7-runtime
+#docker build . -f cuda/Dockerfile.10.2-devel -t timtschu/devenv:cuda10.2-devel
+#docker build . -f cuda/Dockerfile.10.2-cudnn7-devel -t timtschu/devenv:cuda10.2-cudnn7-devel
+
